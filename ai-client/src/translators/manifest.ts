650fdcbdf9bf964b3473f0b2aba4c555368aff4e
Henry Jump <140804846+henryjump@users.noreply.github.com>
2025-09-08 16:19:55 +0100

    remove gemini from ai

--- ai-client/src/translators/manifest.ts
+++ ai-client/src/translators/manifest.ts
@@ -2,12 +2,10 @@ import { manifests as OpenAIConfig } from "./openAI/manifest.js";
 import { manifests as GitHubConfig } from "./github/manifest.js";
 import { manifests as AzureConfig } from "./AzureOpenAI/manifest.js";
 import { manifests as OllamaConfig } from "./ollama/manifest.js";
-import { manifests as GeminiConfig } from "./gemini/manifest.js";
 
 export const manifests = [
   ...OpenAIConfig,
   ...GitHubConfig,
   ...AzureConfig,
   ...OllamaConfig,
-  ...GeminiConfig,
 ];
